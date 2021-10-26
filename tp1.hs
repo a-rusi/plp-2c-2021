6735183b72cd9020161de493e29a81350a60b8a1
a-rusi <alejandro@muttdata.ai>
2021-10-26 20:07:24 -0300

    maybe fixed until exercise 5

--- tp1.hs
+++ tp1.hs
@@ -11,22 +11,15 @@ instance Show a => Show (RTE a) where
 
 
 --------------Resolver--------------
-
 instance Eq a => Eq (RTE a) where
   (Rose i []) == (Rose j []) = i == j
   (Rose i _) == (Rose j []) = False
   (Rose i []) == (Rose j _) = False
-  (Rose i xs) == (Rose j ys) = i == j && compareListsRec xs ys
+  (Rose i hijos1) == (Rose j hijos2) = i == j && length (List.permutations hijos1 `List.intersect` List.permutations hijos2) == length (List.permutations hijos1)
 
-compareListsRec :: (Foldable t, Eq a) => [a] -> t a -> Bool
-compareListsRec [] ys = True
-compareListsRec (x:xs) ys = any (\val -> val == x) ys && compareListsRec xs ys
 
 foldRose:: (a -> [(Char, b)] -> b) -> RTE a -> b
 foldRose f (Rose raiz hijos) = f raiz (map (\(clave, rose) -> (clave ,foldRose f rose)) hijos)
-
-sumaTupla [] = 0
-sumaTupla (x:xs) = snd x + sumaTupla xs
 
 mapRTE :: (a -> b) -> RTE a -> RTE b
 mapRTE f = foldRose (\x rec -> Rose (f x) rec)
@@ -38,19 +31,22 @@ etiquetas :: RTE a -> [Char]
 etiquetas = foldRose (\x rec -> ((fst $ unzip rec) ++ (concat $ snd $ unzip rec)))
 
 altura :: RTE a -> Int
-altura = foldRose (\x rec -> if rec == [] then 0 else 1 + (maximum $ snd $ unzip rec))
+altura = foldRose (\x rec -> if rec == [] then 1 else 1 + (maximum $ snd $ unzip rec))
 
 ramas :: RTE a -> [String]
-ramas = foldRose (\x rec -> if (snd $ unzip rec) == [] 
-    then [fst $ unzip rec]
-    else concat $ map (\ y -> map ((fst y) :) (snd y)) rec)
+ramas = foldRose (\x rec -> if rec == [] then [[]] else concatMap (\ y -> map ((fst y) :) (snd y)) rec)
 
---subRose :: RTE a -> Int -> RTE a
---subRose = foldRose (\x rec -> Rose x rec)
-  --if altura (Rose valor trees) - altura (Rose x rec) = poda
-    --then (Rose x [])
-    --else (Rose x rec))
+--implementacion de subrose utilizando recursion estructural
+subRose2 (Rose raiz hijos) 0 = Rose raiz []
+subRose2 (Rose raiz hijos) poda = Rose raiz (map (\(x, y) -> (x, subRose y (poda-1))) hijos)
 
+--implementacion incompleta de subrose (no funciona)
+subRose unRose poda = (foldRose (\x rec -> if poda <= 0 
+    then (const $ Rose x [])
+    else (aux x rec)) unRose) poda
+
+aux :: Num t => a -> [(Char, t -> RTE a)] -> t -> RTE a
+aux x rec = (\x rec poda -> Rose x (map (\(i,j) -> (i, (j (poda-1)))) rec) ) x rec
 
 tests :: IO Counts
 tests = do runTestTT allTests
@@ -65,35 +61,72 @@ allTests = test [
   ]
 
 unRose = Rose 1 [('a',Rose 2 [('c',Rose 4 [])]),('b',Rose 3 [])]
+otroRose = Rose 2 [('z',Rose 42 [('y',Rose 41 [])]),('x',Rose 40 [])]
+
+arbol1 = Rose 1 [('a', Rose 1 [])]
+arbol2 = Rose 1 [('a', Rose 1 []), ('z', Rose 32 [])]
+
+unRoseConRepetidos = Rose 1 [('a', Rose 2 []), ('a', Rose 2 []), ('b', Rose 2 [])]
+unRoseSinRepetidos = Rose 1 [('a', Rose 2 []), ('b', Rose 2 []), ('b', Rose 2 [])]
+
+arbolOrden1 = Rose 1 [('a', Rose 42 []), ('b', Rose 3 []), ('c', Rose 2 [])]
+arbolOrden2 = Rose 1 [('b', Rose 3 []), ('c', Rose 2 []), ('a', Rose 42 [])]
+
+arbolOrden1ConHijos = Rose 1 [('a', Rose 42 [('a', Rose 200 [])]), ('b', Rose 3 []), ('c', Rose 2 [])]
+arbolOrden2ConHijos = Rose 1 [('b', Rose 3 []),('a', Rose 42 [('a', Rose 200 [])]),  ('c', Rose 2 [])]
+
+
+arbolConAltura3 = Rose 1 [('b', Rose 3 []),('a', Rose 42 [('a', Rose 200 [])]),  ('c', Rose 2 [('z', Rose 99 [])])]
+arbolConAltura4 = Rose 1 [('b', Rose 3 []),('a', Rose 42 [('a', Rose 200 [('y', Rose 1 [])])]),  ('c', Rose 2 [('z', Rose 99 [])])]
+
+arbolMismoValoresDistintasClaves1 = Rose 1 [('a', Rose 2 []), ('a', Rose 2 []), ('b', Rose 2 [])]
+arbolMismoValoresDistintasClaves2 = Rose 1 [('d', Rose 2 []), ('e', Rose 2 []), ('g', Rose 2 [])]
+
+mismoArbolDistintasRaices1 = Rose 42 [('a', Rose 2 []), ('a', Rose 2 []), ('b', Rose 2 [])]
+mismoArbolDistintasRaices2 = Rose 2021 [('a', Rose 2 []), ('a', Rose 2 []), ('b', Rose 2 [])]
+
 
 testsEj1 = test [
-  2 ~=? 1+1,
-  4 ~=? 2*2
+  (Rose 1 [] == Rose 1 []) ~=? True,
+  (Rose 1 [] == Rose 2 []) ~=? False,
+  (unRose == Rose 1 []) ~=? False,
+  (unRose == otroRose) ~=? False,
+  (unRose == unRose) ~=? True,
+  not (arbol1 == arbol2 || arbol2 == arbol1) ~=? True,   --igualdad es simetrica
+  not (unRoseConRepetidos == unRoseSinRepetidos) ~=? True,   --igualdad toma en cuenta repetidos
+  (arbolOrden1 == arbolOrden2) ~=? True, --igualdad toma los contenidos del arbol, sin importar su orden
+  (arbolOrden1ConHijos == arbolOrden2ConHijos) ~=? True,
+  (arbolMismoValoresDistintasClaves1 == arbolMismoValoresDistintasClaves2) ~=? False, --a la igualdad le importan los valores de las claves
+  (mismoArbolDistintasRaices1 == mismoArbolDistintasRaices2) ~=? False
   ]
 
 testsEj2 = test [
-  2 ~=? 1+1,
-  4 ~=? 2*2
+  foldRose (\x rec -> Rose x rec) unRose ~=? unRose,
+  foldRose (\x rec -> if rec == [] then 0 else 1 + (maximum $ snd $ unzip rec)) unRose ~=? 2,
+  mapRTE (\x -> x + 1 ) (Rose 1 []) ~=? Rose 2 []
   ]
 
 testsEj3 = test [
-  2 ~=? 1+1,
-  4 ~=? 2*2
+  nodos (Rose 1 []) ~=? [1],
+  nodos (unRose) ~=? [1,2,4,3],
+  altura (Rose 1 []) ~=? 1,
+  altura (unRose) ~=? 3,
+  altura arbolOrden1ConHijos ~=? 3,
+  altura arbolConAltura3 ~=? 3,
+  altura arbolConAltura4 ~=? 4
   ]
 
 testsEj4 = test [
-  2 ~=? 1+1,
-  4 ~=? 2*2
+  etiquetas (Rose 1 []) ~=? [],
+  etiquetas (unRose) ~=? ['a','b','c']
   ]
 
 testsEj5 = test [
-  2 ~=? 1+1,
-  4 ~=? 2*2
+  ramas (Rose 1 []) ~=? [""],
+  ramas (unRose) ~=? ["ac", "b"]
   ]
 
 testsEj6 = test [
-  2 ~=? 1+1,
-  4 ~=? 2*2
+  subRose2 (Rose 1 []) 1 ~=? Rose 1 [],
+  subRose2 unRose 1 ~=? (Rose 1 [('a',Rose 2 []),('b',Rose 3 [])])
   ]
-
-  --}
